8c78054209fd558c230a2c880563f79384915d73
Arjun-creater <155612773+Arjun-creater@users.noreply.github.com>
2025-03-05 11:35:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import path from 'path'; 
-import { fileURLToPath } from 'url';
+
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Connect to database
 connectDB();
@@ -25,9 +21,6 @@ app.use('/api/users', userRoutes);
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
 
 // Start Server
 const PORT = process.env.PORT || 5000;
