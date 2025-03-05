a710ed671828f8a1a723a7887752bf8aba90e641
Arjun-creater <155612773+Arjun-creater@users.noreply.github.com>
2025-03-05 11:23:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,13 @@ const app = express();
 // Connect to database
 connectDB();
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
