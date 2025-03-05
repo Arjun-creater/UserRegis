1d6af05eac8a684d7e2614ef67a6d689bdda907e
Arjun-creater <155612773+Arjun-creater@users.noreply.github.com>
2025-03-05 11:30:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,36 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import path from 'path'; 
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js'; 
-import process from 'process';
+import userRoutes from './routes/userRoutes.js';
+
 const app = express();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Connect to database
 connectDB();
-const __dirname = path.resolve();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// API Routes
+app.use('/api/users', userRoutes);
+
+
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
-app.use('/api/users', userRoutes);
-
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
